6ac11067b6d0c2b1c399a10a6b8d38356493e69c
Parker <pargrey@gmail.com>
2018-01-03 18:27:05 -0800

    Responsive sizing

--- js.js
+++ js.js
@@ -1,3 +1,4 @@
+// Typed.js Configuration
 var typed = new Typed('.element', {
 strings: ["website.", "portfolio.", "canvas.", "world.", "résumé."],
 typeSpeed: 80,
@@ -8,10 +9,22 @@ loop: true,
 loopCount: Infinity,
 });
 
-
+// Adding functionality to add background to navbar once scrolled past 100px
+/* Currently not working on mobile
+*/
 $(window).scroll(function () {
 if ($(window).scrollTop() >= 100){
   $('.navbar').addClass("scrolled");}
 else{
   $('.navbar').removeClass("scrolled");}
 });
+
+// Making sure my landing is the same size as my browser
+/* Taken from StyleShout Template and modified for production
+*/
+   $('header').css({ 'height': $(window).height()/2 });
+   $(window).on('resize', function() {
+
+        $('header').css({ 'height': $(window).height()/2 });
+        $('body').css({ 'width': $(window).width() })
+   });
