d7cb7eb253836c4448241394bc04bb71906df4f8
Parker <pargrey@gmail.com>
2018-01-04 18:47:41 -0800

    navbar and typed for mobile

--- js.js
+++ js.js
@@ -1,26 +1,35 @@
 // Typed.js Configuration
-var typed = new Typed('.element', {
-strings: ["website.", "portfolio.", "canvas.", "world.", "résumé."],
-typeSpeed: 80,
-backSpeed: 30,
-backDelay: 1200,
-showCursor: false,
-loop: true,
-loopCount: Infinity,
-});
+if ($(window).width() > 480) {
+  var typed = new Typed('.element', {
+  strings: ["website.", "portfolio.", "canvas.", "interests.", "résumé."],
+  typeSpeed: 80,
+  backSpeed: 30,
+  backDelay: 1200,
+  showCursor: false,
+  loop: true,
+  loopCount: Infinity,
+  });
+}
+else {
+  var typed = new Typed('.element', {
+    strings: ["website."],
+    typeSpeed: 80,
+    showCursor: false,
+  });
+}
 
-//Want to stop typed.js if the window is too small
+if ($(window).width() <= 480) {
+  $('.navbar').addClass("scrolled");
+}
+else {
+  $(window).scroll(function () {
+  if ($(window).scrollTop() >= 100){
+    $('.navbar').addClass("scrolled");}
+  else{
+    $('.navbar').removeClass("scrolled");}
+  });
+}
 
 // Adding functionality to add background to navbar once scrolled past 100px
 /* Currently not working on mobile
 */
-$(window).scroll(function () {
-if ($(window).scrollTop() >= 100){
-  $('.navbar').addClass("scrolled");}
-else{
-  $('.navbar').removeClass("scrolled");}
-});
-
-if ((window).width() < 768){
-  $('.navbar').addClass("scrolled");
-}
