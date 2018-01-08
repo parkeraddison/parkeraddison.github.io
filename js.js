00d7137d344925c9352561727c1528ef3342024c
Parker <pargrey@gmail.com>
2018-01-07 17:10:25 -0800

    Projects section, modal

--- js.js
+++ js.js
@@ -18,9 +18,19 @@ else {
   });
 }
 
+//Navbar
 $(window).scroll(function () {
 if ($(window).scrollTop() >= 100){
   $('.navbar').addClass("scrolled");}
 else{
   $('.navbar').removeClass("scrolled");}
 });
+
+$('.navbar a').click(function(){
+    $('.navbar-collapse').collapse('hide');
+});
+
+//modal
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').trigger('focus')
+})
