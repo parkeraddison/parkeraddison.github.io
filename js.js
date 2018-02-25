172b6589ad2a8a7b08a5cd6f87222fbe55cdaad5
Parker <pargrey@gmail.com>
2018-02-24 23:58:56 -0800

    Graphical redo

--- js.js
+++ js.js
@@ -1,26 +1,7 @@
-// Typed.js Configuration
-if ($(window).width() > 480) {
-  var typed = new Typed('.element', {
-  strings: ["website.", "portfolio.", "canvas.", "interests.", "résumé."],
-  typeSpeed: 80,
-  backSpeed: 30,
-  backDelay: 1200,
-  showCursor: false,
-  loop: true,
-  loopCount: Infinity,
-  });
-}
-else {
-  var typed = new Typed('.element', {
-    strings: ["website."],
-    typeSpeed: 80,
-    showCursor: false,
-  });
-}
 
 //Navbar
 $(window).scroll(function () {
-if ($(window).scrollTop() >= 100){
+if ($(window).scrollTop() > 0){
   $('.navbar').addClass("scrolled");}
 else{
   $('.navbar').removeClass("scrolled");}
