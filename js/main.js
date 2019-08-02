fe052d734153da307faf90ec14b657047f050d4a
Parker Grey Addison <pargrey@gmail.com>
2019-08-01 21:12:03 -0700

    Added some color, fixed some animations, changed url to resume.

--- js/main.js
+++ js/main.js
@@ -14,46 +14,6 @@ sr.reveal(".slide-in", {
     easing: 'ease-in-out'
 });
 
-/* Toggle the tooltip on click, close on scroll */
-/*
-function openTooltip() {
-    tooltiptext.style.visibility = "visible";
-    tooltiptext.style.opacity = '1';
-
-    document.addEventListener("click", closeTooltip);
-    document.addEventListener("scroll", closeTooltip);
-};
-
-function closeTooltip() {
-    tooltiptext.style.visibility = "hidden";
-    tooltiptext.style.opacity = '0';
-
-    document.removeEventListener("click", closeTooltip);
-    document.removeEventListener("scroll", closeTooltip);
-};
-
-function toggleTooltip(e) {
-    e.stopImmediatePropagation();
-    if (tooltiptext.style.opacity === '1') {
-        closeTooltip();
-    } else {
-        openTooltip();
-    }
-}
-
-function scrollCloseTooltip() {
-    if (tooltiptext.style.opacity === '1') {
-        closeTooltip();
-    }
-}
-
-// Add event listener for tooltip
-const tooltip = document.getElementById("tooltip");
-const tooltiptext = document.getElementById("tooltiptext");
-tooltip.addEventListener("click", toggleTooltip);
-// tooltip.addEventListener("touchstart", toggleTooltip, false);
-*/
-
 /* Page scrolling */
 function scrollTo(element) {
     window.scroll({
