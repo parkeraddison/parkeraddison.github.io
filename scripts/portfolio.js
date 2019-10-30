3b64a8e22975de9c8bf63c13750d4c1803bebd3e
Parker Grey Addison <pargrey@gmail.com>
2019-10-30 16:23:00 -0700

    Spiffed up portfolio.  Added GPT-2 verbosifier to projects.

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -4,36 +4,51 @@ const md = new remarkable.Remarkable({
 
 let bodyMd = document.getElementById("md");
 
-cv = fetch("/content/portfolio.md")
+let portfolio = fetch("/content/portfolio.md")
     .then(resp => resp.text())
     .then(data => {
         bodyMd.innerHTML = md.render(data);
     })
-    .then(() => tableOfContentify())
+    .then(render)
     .catch(e => {
         throw e;
     });
 
-function tableOfContentify() {
+
+function render() {
 
     let h1s = document.querySelectorAll("#md h1");
+
     let toc = document.getElementById("table-of-contents");
     let tocUl = toc.querySelector("ul");
 
     for (h1 of h1s) {
 
-        let innerText = h1.innerText;
-
-        let displayText = innerText.replace(/\s+\{.*\}/g, '');
-
-        let customId = /\{#(.*)\}/g.exec(innerText);
-        let id = customId ? customId[1] : displayText.replace(/ /g, '-');
-
-        h1.innerText = displayText;
-        h1.setAttribute("id", id);
-
-        tocUl.innerHTML += `<li><a href=#${id}>${toTOCCase(id)}</a></li>`;
+        let id = getHeadingID(h1)
+        tableOfContentify(id, tocUl);
+        sectionify(h1, id);
     }
+
+    colorize()
+}
+
+
+function getHeadingID(h1) {
+    let innerText = h1.innerText;
+
+    let displayText = innerText.replace(/\s+\{.*\}/g, '');
+    h1.innerText = displayText;
+
+    let customId = /\{#(.*)\}/g.exec(innerText);
+    let id = customId ? customId[1] : displayText.replace(/ /g, '-');
+
+    return id
+}
+
+
+function tableOfContentify(id, tocUl) {
+
+    tocUl.innerHTML += `<li><a href=#${id}>${toTOCCase(id)}</a></li>`;
 }
 
 function toTOCCase(string) {
@@ -42,4 +57,85 @@ function toTOCCase(string) {
     string = string.replace(/\b\w/g, char => char.toUpperCase());
 
     return string;
+}
+
+
+function sectionify(h1, id) {
+
+    let elements = descendUntilHeaderOrRule(h1);
+
+    let section = document.createElement("SECTION");
+    section.setAttribute("id", id);
+
+    let content = document.createElement("DIV");
+    content.setAttribute("class", "content");
+
+    let items = divify(elements);
+    addElementsToDiv(content, items);
+
+    h1.insertAdjacentElement("beforebegin", section);
+    section.insertAdjacentElement("beforeend", h1);
+    section.insertAdjacentElement("beforeend", content);
+    
+}
+
+function divify(elements) {
+
+    let divs = [];
+    
+    let h2s = elements.filter(e => e.matches("h2"));
+
+    for (h2 of h2s) {
+
+        let elements = [h2, ...descendUntilHeaderOrRule(h2)];
+
+        let item = document.createElement("DIV");
+        item.setAttribute("class", "item");
+
+        addElementsToDiv(item, elements);
+
+        divs.push(item);
+    }
+
+    return divs;
+}
+
+
+function descendUntilHeaderOrRule(header) {
+
+    let level = +header.tagName[1]
+    let curr = header;
+    let next = curr.nextElementSibling;
+
+    let elements = [];
+
+    while (next != null) {
+        if (next.tagName == "HR" || +next.tagName[1] <= level) {
+            break;
+        } else {
+            elements.push(next);
+            curr = next;
+            next = next.nextElementSibling;
+        }
+    }
+
+    return elements;
+}
+
+function addElementsToDiv(div, elements) {
+    for (element of elements) {
+        div.insertAdjacentElement("beforeend", element);
+    }
+}
+
+function colorize() {
+    let colors = ["#ff6961", "#f0a30a", "#06d6a0", "#00b3e5", "#9560ff"];
+    let items = document.querySelectorAll("#Projects .item");
+
+    for (let i = 0; i < items.length; i++) {
+        item = items[i];
+        color = colors[i % colors.length];
+
+        item.style.borderColor = color;
+    }
 }
