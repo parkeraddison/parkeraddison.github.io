e700c48b9ade2e0450ef68efcc7ce1812831ba3b
Parker Addison <pargrey@gmail.com>
2021-04-19 23:00:20 -0700

    Title classes; remove svg; home js back to index

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,35 +4,11 @@ let darkModeCheckbox;
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    // === Header stickiness ================================================ //
-    const stickyElm = document.querySelector('header')
-
-    const observer = new IntersectionObserver(
-        ([e]) => {
-            e.target.classList.toggle('at-top', e.boundingClientRect.y <= 0);
-        },
-        { threshold: [1] }
-    );
-
-    observer.observe(stickyElm)
-
-    // === Hand waving ====================================================== //
-    const wavingHand = document.querySelector('.wave');
-
-    function waveTheHand() {
-        if (wavingHand.classList.contains('animate-me')) return;
-        wavingHand.classList.add('animate-me');
-        setTimeout(() => wavingHand.classList.remove('animate-me'), 1600);
-    }
-
-    waveTheHand();
-    wavingHand.addEventListener("mouseenter", waveTheHand);
-
-    // === Dark mode ======================================================== //
     root = document.documentElement;
     darkModeCheckbox = document.getElementById('dark-mode');
     darkModeCheckbox.checked = false;
-})
+});
+
 
 function toggleDarkMode() {
     if (root.classList.contains('dark')) {
@@ -44,11 +20,11 @@ function toggleDarkMode() {
     }
 }
 
+function checkedDarkMode() {
+    toggleDarkMode();
+}
+
 if (prefersDarkScheme.matches) {
     toggleDarkMode();
     darkModeCheckbox.checked = true;
-}
-
-function checkedDarkMode() {
-    toggleDarkMode();
 }
